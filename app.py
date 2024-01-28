ed379c1212a4ae6645042c7ddc6f7a86d9e7402c
Szymon <szymon.jan.marek@gmail.com>
2024-01-28 10:29:14 +0100

    main page works and looks ok. it's time for project descroption on project cards

--- app.py
+++ app.py
@@ -15,12 +15,5 @@ def index():
     return render_template('index.html', data=website_data, year=year)
 
 
-@app.route('/testy/')
-def testy():
-    website_data = yaml.load(open('my_data.yaml'), Loader=yaml.SafeLoader)
-    year = datetime.datetime.now().year
-
-    return render_template('idx_testy.html', data=website_data, year=year)
-
 if __name__ == '__main__':
     app.run(debug=True)
